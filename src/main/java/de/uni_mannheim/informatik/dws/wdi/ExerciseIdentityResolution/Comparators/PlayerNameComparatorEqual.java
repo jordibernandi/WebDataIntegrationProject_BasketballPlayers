c331b72436ecde591b564fc13b49fc724de18992
touhidulIslamMilon <43429625+touhidulIslamMilon@users.noreply.github.com>
2022-11-01 22:21:11 +0100

    comit

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Comparators/PlayerNameComparatorEqual.java
@@ -0,0 +1,67 @@
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Comparators;
+
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Movie;
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.PlayerDBpedia;
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.PlayerStat;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.comparators.Comparator;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.comparators.ComparatorLogger;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.similarity.EqualsSimilarity;
+
+public class PlayerNameComparatorEqual implements Comparator<PlayerStat, PlayerDBpedia> {
+
+    private static final long serialVersionUID = 1L;
+    private EqualsSimilarity<String> sim = new EqualsSimilarity<String>();
+
+    private ComparatorLogger comparisonLog;
+
+    @Override
+    public double compare(PlayerStat record1, PlayerStat record2, Correspondence<PlayerDBpedia, Matchable> schemaCorrespondence) {
+        String s1 = record1.getName();
+        String s2 = record2.getName();
+
+        double similarity = sim.calculate(s1, s2);
+
+        if(this.comparisonLog != null){
+            this.comparisonLog.setComparatorName(getClass().getName());
+
+            this.comparisonLog.setRecord1Value(s1);
+            this.comparisonLog.setRecord2Value(s2);
+
+            this.comparisonLog.setSimilarity(Double.toString(similarity));
+        }
+        return similarity;
+    }
+
+    @Override
+    public boolean hasMissingValue(PlayerStat record1, PlayerStat record2, Correspondence<PlayerDBpedia, Matchable> schemaCorrespondence) {
+        return Comparator.super.hasMissingValue(record1, record2, schemaCorrespondence);
+    }
+
+    @Override
+    public PlayerDBpedia getFirstSchemaElement(PlayerStat record) {
+        return Comparator.super.getFirstSchemaElement(record);
+    }
+
+    @Override
+    public PlayerDBpedia getSecondSchemaElement(PlayerStat record) {
+        return Comparator.super.getSecondSchemaElement(record);
+    }
+
+    @Override
+    public ComparatorLogger getComparisonLog() {
+        return Comparator.super.getComparisonLog();
+    }
+
+    @Override
+    public void setComparisonLog(ComparatorLogger comparatorLog) {
+        Comparator.super.setComparisonLog(comparatorLog);
+    }
+
+    @Override
+    public String getName(Correspondence<PlayerDBpedia, Matchable> schemaCorrespondence) {
+        return Comparator.super.getName(schemaCorrespondence);
+    }
+}
